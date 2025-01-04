69ba4a866a2db032266b0d4c50dddf777e6d81f5
Ramesh <rameshchandappayavagal2024@gmail.com>
2025-01-04 13:33:45 +0530

    MVC Project

---
+++ MVCProject/src/main/java/Controller/register.java
@@ -0,0 +1,141 @@
+package Controller;
+import java.io.IOException;
+
+import java.io.PrintWriter;
+
+import Model.Registration;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/register")
+public class register extends HttpServlet {
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        // type of the response sent to the client or browser
+        PrintWriter out = response.getWriter();
+        
+        HttpSession session = request.getSession();
+        Registration reg = new Registration(session);
+        try {
+            if (request.getParameter("register") != null) {
+
+                String name = request.getParameter("name");
+                String phone = request.getParameter("phone");
+                String email = request.getParameter("email");
+                String pw = request.getParameter("pw");
+                String cp = request.getParameter("cp");
+
+                if (pw.equals(cp)) {
+                    String status = reg.Registration(name, phone, email, pw);
+                    
+                    
+                    if (status.equals("existed")) {
+
+                        request.setAttribute("status", "Existed record");
+                        RequestDispatcher rd1 = request.getRequestDispatcher("Registration.jsp");
+                        rd1.forward(request, response);
+
+                    } else if (status.equals("success")) {
+                        request.setAttribute("status", "Successfully Registered");
+                        RequestDispatcher rd1 = request.getRequestDispatcher("Login.jsp");
+                        rd1.forward(request, response);
+
+                    } else if (status.equals("failure")) {
+                        request.setAttribute("status", "Registration failed");
+                        RequestDispatcher rd1 = request.getRequestDispatcher("Registration.jsp");
+                        rd1.forward(request, response);
+
+                    }
+                }
+            } else if (request.getParameter("login") != null) {
+                String email = request.getParameter("email");
+                String pass = request.getParameter("pw");
+                String status = reg.login(email, pass);
+                if (status.equals("success")) {
+
+                    RequestDispatcher rd1 = request.getRequestDispatcher("Index.jsp");
+
+                    rd1.forward(request, response);
+
+                } else if (status.equals("failure")) {
+                    request.setAttribute("status", "Login failed");
+                    RequestDispatcher rd1 = request.getRequestDispatcher("Login.jsp");
+                    rd1.forward(request, response);
+                }
+            } else if (request.getParameter("logout") != null) {
+                session.invalidate();
+                RequestDispatcher rd1 = request.getRequestDispatcher("Index.jsp");
+                rd1.forward(request, response);
+            } 
+            else if (session.getAttribute("uname") != null && request.getParameter("submit") != null) {
+                String name = request.getParameter("name");
+                String pno = request.getParameter("pno");
+                String email = request.getParameter("email");
+                Registration u = new Registration(session);
+                String status = u.update(name, pno, email);
+                if (status.equals("success")) {
+                    request.setAttribute("status", "Profile successfully Updated");
+                    RequestDispatcher rd1 = request.getRequestDispatcher("Index.jsp");
+                    rd1.forward(request, response);
+                } else {
+                    request.setAttribute("status", "Updation failure");
+                    RequestDispatcher rd1 = request.getRequestDispatcher("Index.jsp");
+                    rd1.forward(request, response);
+                }
+            }
+            else if (request.getParameter("forgotPass") != null) {
+                String mail = request.getParameter("email");
+                String pw = request.getParameter("pw");
+                String cp = request.getParameter("cpw");
+                if (pw.equals(cp)) {
+                    String status = reg.ForgotPassword(mail, pw);
+                    if (status.equals("success")) {
+                        request.setAttribute("status", "Password Reset Successfully");
+                        RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+                        rd.forward(request, response);
+                    } else if (status.equals("failure")) {
+                        request.setAttribute("status", "Password Reset Failed");
+                        RequestDispatcher rd = request.getRequestDispatcher("forgot.jsp");
+                        rd.forward(request, response);
+                    }
+                } else {
+                    request.setAttribute("status", "Password mismatch");
+                    RequestDispatcher rd = request.getRequestDispatcher("forgot.jsp");
+                    rd.forward(request, response);
+                }
+            }
+            
+
+        } 
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+   
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+	
+
+}
---
+++ MVCProject/src/main/java/Model/Registration.java
@@ -0,0 +1,209 @@
+package Model;
+import java.sql.*;
+
+
+import java.util.ArrayList;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpSession;
+
+public class Registration {
+	private Connection con;
+    HttpSession se;
+
+    public Registration(HttpSession session) {
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver"); // load the drivers
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sooksmas1", "root", "tiger");
+            // connection with data base
+            se = session;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String Registration(String name, String phone, String email, String pw) {
+        PreparedStatement ps;
+        String status = "";
+        try {
+            Statement st = null;
+            ResultSet rs = null;
+            st = con.createStatement();
+            rs = st.executeQuery("select * from sookshmas1 where phone='" + phone + "' or email='" + email + "';");
+            boolean b = rs.next();
+            if (b) {
+                status = "existed";
+            } else {
+                ps = (PreparedStatement) con.prepareStatement("insert into sookshmas1 values(0,?,?,?,?,now())");
+                ps.setString(1, name);
+                ps.setString(2, phone);
+                ps.setString(3, email);
+                ps.setString(4, pw);
+                int a = ps.executeUpdate();
+                if (a > 0) {
+                    status = "success";
+                } else {
+                    status = "failure";
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return status;
+    }
+
+	public String login(String email, String pass) {
+        String status1 = "", id = "";
+        String name = "", emails = "";
+
+        try {
+            Statement st = null;
+            ResultSet rs = null;
+            st = con.createStatement();
+
+            rs = st.executeQuery("select * from sookshmas1 where email='" + email + "' and password='" + pass + "';");
+            boolean b = rs.next();
+            if (b == true) {
+                id = rs.getString("sino");
+                name = rs.getString("name");
+                emails = rs.getString("email");
+                se.setAttribute("uname", name);
+                se.setAttribute("email", emails);
+                se.setAttribute("id", id);
+                status1 = "success";
+            } else {
+                status1 = "failure";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return status1;
+    }
+	public ArrayList<Student> getUserinfo(String id) { 
+		Statement st = null; 
+		ResultSet rs = null; 
+		ArrayList<Student> al = new ArrayList<Student>(); 
+		try { 
+		st = con.createStatement(); 
+		String qry = "select * from sookshmas1 where sino = '" + id + "';"; 
+		rs = st.executeQuery(qry); 
+		while (rs.next()) { 
+		Student p = new Student(); 
+		p.setId(rs.getString("sino")); 
+		p.setName(rs.getString("name")); 
+		p.setemail(rs.getString("email")); 
+		p.setphone(rs.getString("phone")); 
+		p.setdate(rs.getString("date")); 
+		al.add(p); 
+		} 
+		} catch (Exception e) { 
+		e.printStackTrace(); 
+		} 
+		return al; 
+		}
+	public Student getInfo() {
+        Statement st = null;
+        ResultSet rs = null;
+        Student s = null;
+        try {
+            st = con.createStatement();
+            rs = st.executeQuery("select * from sookshmas1 where sino= '" + se.getAttribute("id") + "'");
+            boolean b = rs.next();
+            if (b == true) {
+                s = new Student();
+                s.setName(rs.getString("name"));
+                s.setphone(rs.getString("phone"));
+                s.setemail(rs.getString("email"));
+            } else {
+                s = null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return s;
+    }
+	public String update(String name, String pno, String email) {
+        String status = "";
+        Statement st = null;
+        ResultSet rs = null;
+        try {
+            st = con.createStatement();
+            st.executeUpdate("update sookshmas1 set name='" + name + "',phone='" + pno + "',email='" + email + "' where sino= '" + se.getAttribute("id") + "' ");
+            se.setAttribute("uname", name);
+            status = "success";
+        } catch (Exception e) {
+            status = "failure";
+            e.printStackTrace();
+        }
+
+        return status;
+    }
+	public ArrayList<Student> getUserDetails() {
+	       Statement st;
+	       ResultSet rs;
+	       ArrayList<Student> al = new ArrayList<Student>();
+	       try {
+	           st = con.createStatement();
+	           String qry = "select *,"
+	                   + "date_format(date,'%b %d, %Y') as date1"
+	                   + " from sookshmas1 where sino not in(1);";
+	           rs = st.executeQuery(qry);
+	           while (rs.next()) {
+	               Student p = new Student();
+	               p.setId(rs.getString("sino"));
+	               p.setName(rs.getString("name"));
+	               p.setemail(rs.getString("email"));
+	               p.setphone(rs.getString("phone"));
+	               p.setdate(rs.getString("date1"));
+	               al.add(p);
+	           }
+	       } catch (Exception e) {
+	           e.printStackTrace();
+	       }
+	       return al;
+	   }
+	public String delete(int id) {
+	       int count = 0;
+	       Statement st = null;
+	       String status = "";
+	       try {
+	           st = con.createStatement();
+	           count = st.executeUpdate("delete from sookshmas1 where "
+	                   + "sino='" + id + "'");
+	           if (count > 0) {
+	               status = "success";
+	           } else {
+	               status = "failure";
+	           }
+	       } catch (Exception e) {
+	           e.printStackTrace();
+	       }
+
+	       return status;
+	   }
+	public String ForgotPassword(String mail, String pw) {
+	       String status = "";
+	       try {
+	           Statement st = con.createStatement();
+
+	           int rspw = st.executeUpdate("update sookshmas1  set password='" + pw + "' where email='" + mail + "';");
+	           if (rspw > 0) {
+	               status = "success";
+	           } else {
+	               status = "failure";
+	           }
+	       } catch (Exception e) {
+	           e.printStackTrace();
+	       }
+	       return status;
+	   }
+
+
+
+
+
+}
